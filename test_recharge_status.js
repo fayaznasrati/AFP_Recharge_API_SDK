1ddf256b127f73f09603e0d30b8b339d7b548562
fayaz nasrati <fayaz.nasraty@gmail.com>
2025-09-02 13:03:04 +0430

    Rechage status for call back status build 2

--- test_recharge_status.js
+++ test_recharge_status.js
@@ -1,5 +1,5 @@
-// const RechargeSDK = require("afp-recharge-api-sdk");
-const RechargeSDK = require("./index.js");
+const RechargeSDK = require("afp-recharge-api-sdk");
+// const RechargeSDK = require("./index.js");
 (async () => {
   try {
     console.log("ENV Rechage:", process.env.API_KEY, process.env.AFP_BASE_URL, process.env.MY_IP_ADDRESS);
